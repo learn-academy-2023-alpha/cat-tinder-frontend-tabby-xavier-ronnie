1aef9e11b9be3d33be260fcef449b115e6d3a22a
Xavier Barker <121983774+XavierB91@users.noreply.github.com>
2023-04-07 15:07:23 -0700

    Update CatIndex.js
    Fixing indentation

--- src/pages/CatIndex.js
+++ src/pages/CatIndex.js
@@ -4,40 +4,38 @@ import { Card, CardBody, CardTitle,CardText,CardSubtitle,Button} from "reactstra
 const CatIndex = ({ cats }) => {
   return (
     
-    <main className="cat-index-cards">
-      {cats?.map((cat, index) => {
-        return (
-          <Card
-  color="danger"
-  outline
-  style={{
-    width: '18rem'
-  }}
->
- 
-    <img alt={`profile of a cat named ${cat.name}`} src={cat.image} />
-  <CardBody>
-    <CardTitle tag="h5">
-     Name: {cat.name}
-    </CardTitle>
-    <CardSubtitle
-      className="mb-2 text-muted"
-      tag="h6"
-    >
-    
-     Age: {cat.age}
-    </CardSubtitle>
-    <CardText>
-    Enjoys: {cat.enjoys}
-    </CardText>
-    <Button>
-     Meow your match ! 🐈
-    </Button>
-  </CardBody>
-</Card>
-        )
-      })}
-    </main>
+   <main className="cat-index-cards">
+  {cats?.map((cat, index) => {
+    return (
+      <Card
+        color="danger"
+        outline
+        style={{
+          width: '18rem'
+        }}
+      >
+        <img alt={`profile of a cat named ${cat.name}`} src={cat.image} />
+        <CardBody>
+          <CardTitle tag="h5">
+            Name: {cat.name}
+          </CardTitle>
+          <CardSubtitle
+            className="mb-2 text-muted"
+            tag="h6"
+          >
+            Age: {cat.age}
+          </CardSubtitle>
+          <CardText>
+            Enjoys: {cat.enjoys}
+          </CardText>
+          <Button>
+            Meow your match ! 🐈
+          </Button>
+        </CardBody>
+      </Card>
+    )
+  })}
+</main>
   )
 }
 
